ae0d7e19b75bfbc5cff24bdfd4d48e39d4e98005
gk <guoguo882010@hotmail.com>
2018-09-08 16:46:09 +0800

    test

--- WebScan/Program.cs
+++ WebScan/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using MadMilkman.Ini;
 
 namespace GK.WebScan
 {
@@ -11,6 +12,22 @@ namespace GK.WebScan
     {
         static void Main(string[] args)
         {
+            // Create new file with a default formatting.
+            IniFile file = new IniFile();
+
+            // Add new section.
+            IniSection section = file.Sections.Add("Section name");
+            // Add trailing comment.
+            section.TrailingComment.Text = "Section trailing comment";
+
+            // Add new key and its value.
+            IniKey key = section.Keys.Add("Key name", "Key value ");
+            // Add leading comment.
+            key.LeadingComment.Text = "Key leading comment";
+
+            // Save file.
+            file.Save("Sample.ini");
+
             Proxy proxy = new Proxy(@"C:\Users\guogu\Desktop\1.txt",true);
 
             while (true)
